cd6eee190c158a8aa4ff669b7269b10b275d0b7b
Carles Far Pierres <carlesfarpierres@MacBook-Pro-de-Carles.local>
2023-05-23 00:55:22 +0200

    updates

---
+++ client/.nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/carlesfarpierres/Documents/workspace/app-node-vue/client/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
---
+++ client/.nuxt/types/schema.d.ts
@@ -0,0 +1,30 @@
+import { NuxtModule } from '@nuxt/schema'
+declare module '@nuxt/schema' {
+  interface NuxtConfig {
+    ["pinia"]?: typeof import("@pinia/nuxt").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    ["piniaPersistedstate"]?: typeof import("@pinia-plugin-persistedstate/nuxt").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+    ["telemetry"]?: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
+  }
+  interface RuntimeConfig {
+   app: {
+      baseURL: string,
+
+      buildAssetsDir: string,
+
+      cdnURL: string,
+   },
+  }
+  interface PublicRuntimeConfig {
+   keyClodinary: string,
+
+   apiKey: string,
+
+   persistedState: {
+      storage: string,
+
+      debug: boolean,
+
+      cookieOptions: any,
+   },
+  }
+}
