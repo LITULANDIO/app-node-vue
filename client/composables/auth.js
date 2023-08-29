2ea35df01aa2b6b7b06fdd9dab7e23b3e64aa0c8
Carles Far Pierres <carlesfarpierres@MacBook-Pro-de-Carles.local>
2023-08-29 19:06:12 +0200

    export deletecookies

--- client/composables/auth.js
+++ client/composables/auth.js
@@ -31,9 +31,21 @@ export default () => {
 
     const clearAuthData = () => deleteToken()
 
+    const deleteAllCookies = () => {
+        const cookies = document.cookie.split(";");
+      
+        for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i];
+          const eqPos = cookie.indexOf("=");
+          const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+          document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;";
+        }
+    }
+
     return {
         isAuthenticated,
         setToken,
-        clearAuthData
+        clearAuthData,
+        deleteAllCookies
     }
 }
