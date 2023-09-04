ea109f76e53ff81c43937fd07b2b0d980273a197
Carles Far Pierres <carlesfarpierres@MacBook-Pro-de-Carles.local>
2023-09-04 20:10:58 +0200

    fix button config

--- client/app.vue
+++ client/app.vue
@@ -2,7 +2,7 @@
     <div v-if="loading" class="fixed left-0 top-0 h-0.5 w-full z-50 bg-green-500" ></div>
     <NuxtPage />
     <NuxtLoadingIndicator />
-    <ButtonConfig/>
+    <ButtonConfig v-if="!loading"/>
 </template>
 
 <script setup>
