24efdc6867cdc708f5610000850ed8326e13ac17
Carles Far Pierres <carlesfarpierres@MacBook-Pro-de-Carles.local>
2023-09-05 16:48:45 +0200

    fixes

--- client/pages/dashboard/user/group/my-friend/[id].vue
+++ client/pages/dashboard/user/group/my-friend/[id].vue
@@ -1,47 +1,52 @@
 <template>
     <NuxtLayout/>
-    <div class="pt-4">
-        <BackButton />
-        <ButtonInfo />
+    <div  v-if="friend.friend.name">
+        <Spinner/>
     </div>
-    <section id="options" class="flex justify-center items-center flex-col">
-        <Button :label="$t('pages.myFriend.field1')" @click="onViewFriend"/>
-        <Transition name="slide-fade">
-            <section class="view-friend" v-if="isViewFriend && friend.friend.name">
-                <Modal :show="isViewFriend" @onClose="onCloseViewFriend" padding>
-                    <h1 class="capitalize text-center mb-5">{{ friend.friend.name }}</h1>
-                    <div class="container-photo mb-3">
-                        <img :src="friend.friend.photo" :alt="friend.friend.name" />
-                    </div>
-                </Modal>
-            </section>
-        </Transition>
-        <Button :label="$t('pages.myFriend.field2')" @click="onViewWishesFriend"/>
-        <Transition name="slide-fade">
-            <section class="view-friend" v-if="isViewWishesFriend">
-                <Modal :header="`${$t('pages.myFriend.headerModal')} ${friend.friend.name}`" :show="isViewWishesFriend" @onClose="onCloseViewWishesFriend" padding>
-                    <ul v-if="!isFriendWishesEmpty" class="mt-3">
-                        <li v-for="wish in friendWishes">
-                            <font-awesome-icon v-if="wish" icon="fas fa-gift" /> 
-                            <span class="ml-2">{{ wish }}</span>
-                        </li>
-                    </ul>
-                    <div class="flex justify-center" v-else>{{ $t('pages.myFriend.message') }} 🤷</div>
-                </Modal>
-            </section>
-        </Transition>
-        <Button :label="$t('pages.myFriend.field3')" @click="onViewFormWishes"/>
-        <Transition name="slide-fade">
-            <section class="view-friend" v-if="isViewWishesMe">
-                <FormWishes 
-                    :isOpen="isOpenModal"
-                    @onClose="onCloseModal"
-                    :data="data"
-                    :wishes="friend.me.wishes"
-                />
-            </section>
-        </Transition>
-    </section>
+    <div v-else>
+        <div class="pt-4">
+            <BackButton />
+            <ButtonInfo />
+        </div>
+        <section id="options" class="flex justify-center items-center flex-col">
+            <Button :label="$t('pages.myFriend.field1')" @click="onViewFriend"/>
+            <Transition name="slide-fade">
+                <section class="view-friend" v-if="isViewFriend">
+                    <Modal :show="isViewFriend" @onClose="onCloseViewFriend" padding>
+                        <h1 class="capitalize text-center mb-5">{{ friend.friend.name }}</h1>
+                        <div class="container-photo mb-3">
+                            <img :src="friend.friend.photo" :alt="friend.friend.name" />
+                        </div>
+                    </Modal>
+                </section>
+            </Transition>
+            <Button :label="$t('pages.myFriend.field2')" @click="onViewWishesFriend"/>
+            <Transition name="slide-fade">
+                <section class="view-friend" v-if="isViewWishesFriend">
+                    <Modal :header="`${$t('pages.myFriend.headerModal')} ${friend.friend.name}`" :show="isViewWishesFriend" @onClose="onCloseViewWishesFriend" padding>
+                        <ul v-if="!isFriendWishesEmpty" class="mt-3">
+                            <li v-for="wish in friendWishes">
+                                <font-awesome-icon v-if="wish" icon="fas fa-gift" /> 
+                                <span class="ml-2">{{ wish }}</span>
+                            </li>
+                        </ul>
+                        <div class="flex justify-center" v-else>{{ $t('pages.myFriend.message') }} 🤷</div>
+                    </Modal>
+                </section>
+            </Transition>
+            <Button :label="$t('pages.myFriend.field3')" @click="onViewFormWishes"/>
+            <Transition name="slide-fade">
+                <section class="view-friend" v-if="isViewWishesMe">
+                    <FormWishes 
+                        :isOpen="isOpenModal"
+                        @onClose="onCloseModal"
+                        :data="data"
+                        :wishes="friend.me.wishes"
+                    />
+                </section>
+            </Transition>
+        </section>
+    </div>
 </template>
 
 <script setup>
