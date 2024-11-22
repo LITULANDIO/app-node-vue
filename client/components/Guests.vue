d7a31a840a03cb8b43199caf2a4dcb3deef1e22b
Litus <carlesfarpierres@MBP-de-Carles.station>
2024-11-22 12:41:24 +0100

    fixed sintax

--- client/components/Guests.vue
+++ client/components/Guests.vue
@@ -154,7 +154,7 @@
   </Button>
   <Modal :show="showGuestList" @onClose="onCloseGuestList" padding>
     <div v-for="guests in guestsList" :key="guests.id">
-      <span class="mx-3">Total invisibles: {{ guestList.length }} </span>
+      <span class="mx-3">Total invisibles: {{ guestsList.length }} </span>
       <input
         type="checkbox"
         :id="guests.id"
