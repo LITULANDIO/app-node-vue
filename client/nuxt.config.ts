1eb50283f702e808a35e159cd2479096a0807e8f
Litus <carlesfarpierres@MBP-de-Carles.station>
2025-01-14 19:47:03 +0100

    updated meta description

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -2,32 +2,45 @@
 // require('dotenv').config()
 
 export default defineNuxtConfig({
-    runtimeConfig: {
-        public: {
-            keyClodinary: process.env.KEY_CLOUDINARY,
-            apiKey: process.env.API_URL
-        }
-      },
-    //   vite: process.env.NODE_ENV === 'development' ? {
-    //     server: {
-    //         hmr: {
-    //             port: 4001,
-    //             protocol: 'wss',
-    //             host: 'localhost',
-    //         }
-    //     }
-    // } : {},
-    css: ['~/assets/css/main.css','@fortawesome/fontawesome-svg-core/styles.css'],
-    postcss: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
+  app: {
+    head: {
+      title: "Carles Far - Desarrollo Web y Consultoría",
+      meta: [
+        {
+          name: "description",
+          content:
+            "Carles Far, experto en desarrollo web, ofrece soluciones a medida para empresas y emprendedores.",
         },
+      ],
     },
-    ssr: false,
-    build: {
-        transpile: [
-          "@urql/vue"
-        ], 
-      },
-})
+  },
+  runtimeConfig: {
+    public: {
+      keyClodinary: process.env.KEY_CLOUDINARY,
+      apiKey: process.env.API_URL,
+    },
+  },
+  //   vite: process.env.NODE_ENV === 'development' ? {
+  //     server: {
+  //         hmr: {
+  //             port: 4001,
+  //             protocol: 'wss',
+  //             host: 'localhost',
+  //         }
+  //     }
+  // } : {},
+  css: [
+    "~/assets/css/main.css",
+    "@fortawesome/fontawesome-svg-core/styles.css",
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  ssr: false,
+  build: {
+    transpile: ["@urql/vue"],
+  },
+});
