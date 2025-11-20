555eaad9aa296c0788ac6af6b7d6e103ece8d974
Litus <carlesfarpierres@MacBook-Pro-de-Carles.local>
2025-11-20 19:36:50 +0100

    updated date

--- client/components/ButtonInfo.vue
+++ client/components/ButtonInfo.vue
@@ -60,7 +60,7 @@ const onClose = () => {
 //#methods
 const changeFormatDate = (date) => {
   const day = new Date(date).getDate();
-  const month = new Date(date).getMonth();
+  const month = new Date(date).getMonth() + 1;
   const year = new Date(date).getFullYear();
   return `${day}/${month}/${year}`;
 };
